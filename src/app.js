c698331acd86227e8dfd74bb1c89995d422f2185
Abinaya <141475809+481n4y4@users.noreply.github.com>
2026-02-08 08:52:10 +0700

    ✅ fix(api): fix route api app.js

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/todos", todoRoutes);
+app.use("/auth", authRoutes);
+app.use("/todos", todoRoutes);
 
 app.get("/", (req, res) => {
   res.send("API MindFlow is running...");
